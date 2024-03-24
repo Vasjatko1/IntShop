26e18bf32df25e36eb242d5c802375c959c1b7a6
Student4 <olexandrborysenko@gmail.com>
2024-03-24 11:46:53 +0200

    first load

--- js/script.js
+++ js/script.js
@@ -36,3 +36,38 @@ $(document).ready(function () {
     });
     $(".collapsible").collapsible();
 });
+$(document).ready(function () {
+    $(".modal").modal();
+});
+window.addEventListener("click", (event) => {
+    console.log("click");
+    if (event.target.classList.contains("button-buy")) {
+        console.log("клік на кнопку");
+        addItems(event.target);
+    }
+    if (event.target.classList.contains("delete-items")) {
+        console.log("клік на кнопку видалення товару");
+        deleteItems(event.target);
+    }
+});
+function addItems(element) {
+    console.log("додавання товару");
+    let li = document.createElement("li");
+    li.innerHTML =
+        element.dataset.img +
+        element.dataset.name +
+        element.dataset.price +
+        '<div class="delete-items"> X </div>';
+    cart.appendChild(li);
+    addCartToForm(element.dataset.id);
+    M.toast({ html: "товар у кошику", classes: "rounded" });
+}
+function deleteItems(element) {
+    console.log("видалення товару");
+    element.parentNode.remove();
+}
+let inputCart = document.getElementById("cartFF");
+
+function addCartToForm(id) {
+    inputCart.value += id + ",";
+}
